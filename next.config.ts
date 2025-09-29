d01ccd4efb5fa1d73682cfd8fa35b9dbf4a5a863
Kevin B <93852813+retrobone@users.noreply.github.com>
2025-09-29 20:23:26 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
+// File: retrobone/tripsters-innov/tripsters-innov-25d0006c070fe2f6384c93b56a848a48898dcf98/next.config.ts
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Tells Next.js to create a static export
   output: 'export',
 
-  // This is the crucial line to add.
-  // It must match your repository name.
+  // Tells Next.js the site will be in a subfolder
   basePath: '/tripsters-innov',
 
   images: {
